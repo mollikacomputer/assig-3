c38c2fb14cd9069b8dbd15f85c092d139ff73257
mollikacomputer <mollikacomputer3@gmail.com>
2026-06-17 23:00:16 +0600

    answer to the questions 3 or query 3

--- query.sql
+++ query.sql
@@ -73,4 +73,11 @@ SELECT match_id, fixture, base_ticket_price
 SELECT user_id, full_name, email FROM users
 WHERE full_name ILIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
-   
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+
+SELECT booking_id, user_id, match_id,
+    COALESCE(payment_status, 'Action Required') AS payment_status,
+    total_cost
+FROM bookings
+WHERE payment_status IS NULL;
