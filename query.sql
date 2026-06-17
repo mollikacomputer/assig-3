e4a61bb3e8e800bbb08e2c22691dff2df61a1f69
mollikacomputer <mollikacomputer3@gmail.com>
2026-06-17 21:56:07 +0600

    insert data into users table

--- query.sql
+++ query.sql
@@ -9,3 +9,9 @@ CREATE TABLE users (
     phone_number VARCHAR(20) UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+-- insert data into users table
+INSERT INTO users(full_name, email, role, phone_number) VALUES
+  ('Tanvir Rahman', 'info@tabvir.com', 'Football Fan', +8801914924473),
+  ('Asif Haque', 'info@asif.com', 'Football Fan', +8801910000473),
+  ('Sajjad Rahman', 'info@sajjad.com', 'Football Fan', +8801714924473),
+  ('Jannat Ara', 'info@jannat.com', 'Football Fan', +8801614924473);
