3cf86ad2a8da0dd55fda73bf59de166450199b82
mollikacomputer <mollikacomputer3@gmail.com>
2026-06-17 21:30:27 +0600

    next assignment3

---
+++ query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Users (
+    user_id TYPE,
+    full_name TYPE,
+    email TYPE,
+    role TYPE,
+    phone_number TYPE,
+    
+    -- Write your constraint to make 'user_id' the Primary Key
+    -- Write your constraint to ensure 'email' values are never duplicated
+    -- Write your check constraint to restrict 'role' to specific allowed strings
+);
