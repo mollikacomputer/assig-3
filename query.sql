1460110fe22750ae4ab129907fe9d86eded0c736
mollikacomputer <mollikacomputer3@gmail.com>
2026-06-17 21:47:16 +0600

    created users table

--- query.sql
+++ query.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Users (
-    user_id TYPE,
-    full_name TYPE,
-    email TYPE,
-    role TYPE,
-    phone_number TYPE,
-    
-    -- Write your constraint to make 'user_id' the Primary Key
-    -- Write your constraint to ensure 'email' values are never duplicated
-    -- Write your check constraint to restrict 'role' to specific allowed strings
+-- Create users Table
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    role VARCHAR(20) NOT NULL CHECK (
+        role IN ('Ticket Manager', 'Football Fan')
+    ),
+    phone_number VARCHAR(20) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
